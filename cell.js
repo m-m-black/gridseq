585b687037174fb199f9a79d8f45ff3a039ee139
m-m-black <morganblack1988@gmail.com>
2020-12-26 19:58:01 +1100

    Cells now activate on mousePressed

--- cell.js
+++ cell.js
@@ -4,7 +4,8 @@ class Cell {
 		this.y = y;
 		this.size = size;
 		this.highlighted = false;
-		this.active = false;
+		this.active = false; // Cell has been clicked on
+		this.canActivate = true; // Cell can be clicked on
 		this.color = color(200, 0, 0);
 	}
 
@@ -14,28 +15,41 @@ class Cell {
 	}
 
 	highlight() {
-		this.color = color(0, 200, 0);
+		if (!this.active) {
+			this.color = color(0, 200, 0);
+		}
 	}
 
 	dehighlight() {
-		this.color = color(200, 0, 0);
+		if (!this.active) {
+			this.color = color(200, 0, 0);
+		}
 	}
 
 	activate() {
+		this.color = color(0, 0, 200);
 		this.active = true;
 	}
 
 	deactivate() {
+		this.color = color(200, 0, 0);
 		this.active = false;
+	}
+
+	block() {
+		this.canActivate = false;
+	}
+
+	unblock() {
+		this.canActivate = true;
 	}
 
 	within(mouseX, mouseY) {
 		let within = false;
 		if (abs(mouseX - this.x) < (this.size / 2) && abs(mouseY - this.y) < (this.size / 2)) {
-			this.highlight();
 			within = true;
 		} else {
-			this.dehighlight();
+		
 			within = false;
 		}
 		return within;
--- sketch.js
+++ sketch.js
@@ -27,6 +27,7 @@ function draw() {
 			cell.display();
 			let within = cell.within(mouseX - width / 2, mouseY - height / 2);
 			if (within) {
+				cell.highlight();
 				grid.rows[rowIndex].forEach(c => {
 					c.highlight();
 					c.display();
@@ -35,13 +36,35 @@ function draw() {
 					c.highlight();
 					c.display();
 				})
+			} else {
+				cell.dehighlight();
 			}
 		})
 	})
 }
 
 function mousePressed() {
+	cells.forEach((row,rowIndex) => {
+		row.forEach((cell, cellIndex) => {
+			if (cell.within(mouseX - width / 2, mouseY - height / 2)) {
+				if (cell.canActivate) {
+					cell.activate();
+					blockCells(rowIndex, cellIndex);
+				}
+			}
+		})
+	})
 	return false;
+}
+
+// Prevent specified cells from being able to activate
+function blockCells(rowIndex, colIndex) {
+	grid.rows[rowIndex].forEach(cell => {
+		cell.block();
+	})
+	grid.columns[colIndex].forEach(cell => {
+		cell.block();
+	})
 }
 
 function initCells() {
